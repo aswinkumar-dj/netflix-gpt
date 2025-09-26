2e5e494dfad90f3202848e63b2f81298d043bc0a
aswinkumar-dj <203110173+aswinkumar-dj@users.noreply.github.com>
2025-09-26 22:49:52 +0530

    test

--- src/utils/constant.jsx
+++ src/utils/constant.jsx
@@ -1,2 +1,11 @@
 export const BG_LOGIN =
   "https://assets.nflxext.com/ffe/siteui/vlv3/c95abc7a-8124-4630-bb7a-3b160bdc6de3/web/IN-en-20250915-TRIFECTA-perspective_d3d87aa7-58ed-4c6b-98dc-231ed05ba675_large.jpg";
+
+export const API_OPTIONS = {
+  method: "GET",
+  headers: {
+    accept: "application/json",
+    Authorization:
+      "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJjYmQzNWMyMzQ1ODY1ZTBjZWQxMjYwMWQwNzFjMjU2ZSIsIm5iZiI6MTc1ODg5NjU3NS43MjcsInN1YiI6IjY4ZDZhMWJmMzM3ZWI4MWMyZmM4MzZkNSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.sDYyUXJ8zVcqxgFOTVNZqRrQ2Shl-4NWxVW0EJ95plQ",
+  },
+};
