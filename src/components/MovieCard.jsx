e92a3a0fa6e26d5c946deb16dc023464537beba5
aswinkumar-dj <203110173+aswinkumar-dj@users.noreply.github.com>
2025-09-30 15:39:05 +0530

    hover card

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -3,8 +3,15 @@ import { POSTER_CDN } from "../utils/constant";
 
 const MovieCard = ({ posterPath }) => {
   return (
-    <div className="w-40 hover:animate-pulse">
-      <img alt="movie-poster" src={POSTER_CDN + posterPath} />
+    <div
+      className="w-40 cursor-pointer transform transition duration-300 
+                 hover:scale-110 hover:z-10"
+    >
+      <img
+        alt="movie-poster"
+        src={POSTER_CDN + posterPath}
+        className="rounded-md shadow-md hover:shadow-xl"
+      />
     </div>
   );
 };
